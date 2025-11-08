19747947608f2c3c439efb31f76f809b78c1899e
kelly103312 <cristaldemilcolores@gmail.com>
2025-11-08 18:51:51 -0500

    Cambios en local para probar enemigo

--- Wiskers/src/enemies/GhostEnemy.js
+++ Wiskers/src/enemies/GhostEnemy.js
@@ -4,30 +4,30 @@ export class GhostEnemy extends EnemyBase {
     constructor(scene, x, y) {
         console.log('[GhostEnemy] constructor called with', x, y);
         super(scene, x, y, 'ghost');
-
-        this.setScale(0.8);
+        this.setScale(1.0);
         this.setAlpha(0.8);
-        this.body.setSize(160, 160);
+        this.body.setSize(160, 200);
         this.body.setAllowGravity(false);
-        this.body.setOffset(20, 20);
-
+        this.body.setOffset(30, 30);
         this.play('ghost-float');
         this.patrol({ fromX: x - 100, toX: x + 100 });
     }
 
     static preload(scene) {
-        scene.load.spritesheet('ghost', 'assets/enemy/gatoInvisibleFinal.png', {
-            frameWidth: 200,
-            frameHeight: 200
+        scene.load.spritesheet('ghost', 'assets/enemy/gatito.png', {
+            frameWidth: 128,
+            frameHeight: 128
         });
     }
 
     static createAnimations(scene) {
         scene.anims.create({
             key: 'ghost-float',
-            frames: scene.anims.generateFrameNumbers('ghost', { start: 0, end: 24 }),
-            frameRate: 12,
+            frames: scene.anims.generateFrameNumbers('ghost', { start: 0, end: 35 }),
+            frameRate: 3,
             repeat: -1
         });
+        
     }
+    
 }
